d3a00a0d76c73ccfd63efbb441f4912a0c8308c3
redinmay <redinmay@gmail.com>
2021-05-13 03:19:45 +0900

    2nd commit add file and edit st.py

--- st.py
+++ st.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-plt.rc("font", family="Malgun Gothic")
+#plt.rc("font", family="Malgun Gothic")
 
 
 st.title(":musical_score: BROADCAST DATA REPORT")
